c2849e4971494396eca831b7c53e9228de10a04d
philippe-nat <plitou@natsystem.fr>
2022-12-02 19:31:06 +0100

    syracuse_lambda_ok

--- Noyau.php
+++ Noyau.php
@@ -4,4 +4,8 @@ class Noyau {
     public static function echoln($s):void {
         echo $s . '<br/>';
     }
+    public static function echoErrln($s):void {
+        echo '<div style="color:#ff0000">' . $s . '</div><br/>';
+    }
+
 }
