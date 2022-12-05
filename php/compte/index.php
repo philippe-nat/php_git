8cd49648a69d73b66f37d5f35ea5af314ee7620b
philippe-nat <plitou@natsystem.fr>
2022-12-05 18:54:23 +0100

    page de login

--- php/compte/index.php
+++ php/compte/index.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Connexiont</title>
+    <title>Connexion</title>
+    <link rel="stylesheet" href="ergonomie/css/style.css">
 </head>
 <body>
     <section>
@@ -14,6 +15,7 @@
             <input type="text" name="pass " id="pass" placeholder="mot de passe"/>
             <input type="submit" name="créer" value="Créer un compte"/>
         </form>
+        <a href="">Accès compte</a>
     </section>
 </body>
 </html>
